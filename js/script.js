2f30a1c64be326a99e3bad9bfd1a26bc44b077cd
JhonatanDczel <jariasq@unsa.edu.pe>
2023-11-15 15:05:51 -0500

    Redefine la estructura de los botones

--- js/script.js
+++ js/script.js
@@ -1,22 +1,10 @@
 //Variables globales
+const buttons = document.querySelector('.buttons');
 const numPad = document.querySelector('.numpad');
 const toolBar = document.querySelector('.tool-bar');
 const operations = document.querySelector('.operations');
 
 //Creacion de interfaz
-for (let i = 0; i < 10; i++) {
-  const numBtn = document.createElement('button');
-  numBtn.textContent = 9 - i;
-  numPad.appendChild(numBtn);
-  numBtn.classList.add('button');
-  if (numBtn.textContent == 0) numBtn.classList.add('horizontal');
-  if (i == 9) {
-    const dotBtn = document.createElement('button');
-    dotBtn.textContent = '.';
-    numPad.appendChild(dotBtn);
-    dotBtn.classList.add('button');
-  }
-}
 
 for (let i = 0; i < 3; i++) {
   const toolBtn = document.createElement('button');
@@ -25,22 +13,35 @@ for (let i = 0; i < 3; i++) {
     case 0:
       toolBtn.textContent = 'Ac';
       break;
-
     case 1:
       toolBtn.textContent = 'Del';
       break;
-
     case 2:
       toolBtn.textContent = 'C';
       break;
   }
-  toolBar.appendChild(toolBtn);
+  buttons.appendChild(toolBtn);
 }
+
+for (let i = 0; i < 10; i++) {
+  const numBtn = document.createElement('button');
+  numBtn.textContent = 9 - i;
+  buttons.appendChild(numBtn);
+  numBtn.classList.add('button');
+  if (numBtn.textContent == 0) numBtn.classList.add('horizontal');
+  if (i == 9) {
+    const dotBtn = document.createElement('button');
+    dotBtn.textContent = '.';
+    buttons.appendChild(dotBtn);
+    dotBtn.classList.add('button');
+  }
+}
+
 
 for (let i = 0; i < 5; i++) {
   const operatorBtn = document.createElement('button');
   let symbols = ['/', '*', '-', '+', '='];
   operatorBtn.textContent = symbols[i];
   operatorBtn.classList.add('button');
-  operations.appendChild(operatorBtn);
+  buttons.appendChild(operatorBtn);
 }
